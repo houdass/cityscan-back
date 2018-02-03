36346426bb9b2442ee55f9e6486e8f98aa8b726b
Houdass Youness <houd.youness@gmail.com>
2018-02-03 22:39:20 +0000

    add babel-plugin-add-module-exports

--- src/config/gulp.config.js
+++ src/config/gulp.config.js
@@ -1,8 +1,6 @@
-const GULP_CONFIG = {
+export default {
   SRC_FILES: ['./src/**/*.js'],
   BUILD_FILES: ['./build/**/*.js'],
   TEST_FILES: ['**/*.spec.js', '**/*.integration.js'],
   ROUTES_FILES: './src/routers/**/*.js'
 };
-
-module.exports = GULP_CONFIG;
--- src/config/main.config.js
+++ src/config/main.config.js
@@ -1,4 +1,4 @@
-const MAIN_CONFIG = {
+export default {
   // Secret key for JWT signing and encryption
   SECRET: 'hiit-cityscan-secret-passphrase',
   EXPIRES_IN: 86400,
@@ -6,9 +6,8 @@ const MAIN_CONFIG = {
   MONGODB_URI: 'mongodb://demo:demo@ds251737.mlab.com:51737/cityscan',
   // Setting port for server
   PORTS: {
-    EXPRESS: 5050,
-    BROWSER_SYNC: 3000,
-    NODEMON: 8000
+    EXPRESS: 8080,
+    BROWSER_SYNC: 3000
   },
   ENVS: {
     DEV: 'DEV',
@@ -16,5 +15,3 @@ const MAIN_CONFIG = {
     TEST: 'TEST'
   }
 };
-
-module.exports = MAIN_CONFIG;
